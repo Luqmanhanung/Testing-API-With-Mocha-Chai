fb846498f29d7c4b1d2bf4b40dc99372b8f9f880
Luqman Hanung Asidiq <luqmanhanung@gmail.com>
2022-12-03 12:02:22 +0700

    Belajar Mocha and chai awal

--- test/get_neows_test.js
+++ test/get_neows_test.js
@@ -3,7 +3,8 @@ const assert = require('chai').expect;
 chai.use(require('chai-json-schema'));
 
 const page = require('../page/get_neows_page');
-const schema = require('../data/schema.json')
+const schemavalid = require('../data/schemavalid.json')
+const schemainvalid = require('../data/schemainvalid.json')
 
 let payload = {
     'start_date'    : '2022-12-01',
@@ -16,7 +17,10 @@ const testCase={
     positive :{
         validParams:'As User I want to see object near earth'
     },
-    negative :{
+    negative1 :{
+        invalidParamsdate:'As User I want to see object near earth before today'
+    },
+    negative2 :{
         invalidParams: 'As User I  want to see object near earth more than 7 days'
     }
 }
@@ -25,12 +29,21 @@ describe(`@get ${testCase.describe}`, () => {
     it(`@positive ${testCase.positive.validParams}`, async () => {
         const response = await page.getNeows(payload);
         assert(response.status).to.equal(200);
-        assert(response.body).to.be.jsonSchema(schema);
+        assert(response.body).to.be.jsonSchema(schemavalid);
     });
 
-    it(`@negative ${testCase.negative.invalidParams}`, async () => {
+    it(`@negative ${testCase.negative2.invalidParams}`, async () => {
         payload.end_date ='2023-01-01';
         const response = await page.getNeows(payload);
         assert(response.status).to.equal(400);
+        assert(response.body).to.be.jsonSchema(schemainvalid);
+    });
+
+    it(`@negative ${testCase.negative1.invalidParamsdate}`, async () => {
+        payload.start_date = '2022-01-0'
+        payload.end_date ='2022-01-0';
+        const response = await page.getNeows(payload);
+        assert(response.status).to.equal(400);
+        assert(response.body).to.be.jsonSchema(schemainvalid);
     });
 });
